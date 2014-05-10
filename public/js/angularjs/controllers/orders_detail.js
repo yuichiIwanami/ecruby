07dc6f6a0572b604d9e8f4edb7d23a5faa225edd
yuichi <gaudi8@gmail.com>
2014-05-11 04:13:36 +0900

    order detail product

--- public/js/angularjs/controllers/orders_detail.js
+++ public/js/angularjs/controllers/orders_detail.js
@@ -1,9 +1,38 @@
 'use strict';
 
+var Product = function(id, name, price, qty) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.qty = qty;
+    this.tax = function() {
+        return this.price*0.08;
+    };
+    this.subtotal = function() {
+        return (Number(this.price)+Number(this.tax()))*this.qty;
+    }
+};
+
 var ordersDetail = angular.module('ordersDetail', []).
     controller('ordersDetailCtrl', function($scope) {
         $scope.products = [
-            {id: "001", name: "book", price: 1000, quantity: 1, tax:80}
+            new Product("001", "book", 1000, 1),
+            new Product("002", "pen", 500, 1)
         ];
-        $scope.test;
+
+        $scope.discount = '';
+        $scope.$watch('products', function() {
+            var sumtotal = 0;
+//            var grandtotal = 0;
+
+            $scope.products.forEach(function(product) {
+                sumtotal += product.subtotal();
+            });
+            $scope.sumtotal = sumtotal;
+
+            $scope.grandtotal = function(){
+                return sumtotal - $scope.discount;
+            }
+
+        }, true);
     });
